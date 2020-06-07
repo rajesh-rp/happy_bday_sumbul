ec6ff9a7b742b29889ee0147a08adcc81cc6fd81
rajesh pradhan <pradhanrajesh845@gmail.com>
2020-06-07 12:34:13 +0530

    added play and pause buttons

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,7 @@
-$(".wish-button").on("click",function(){
-    var video=$("video")[0].play();
+var video=$("video")[0];
+$(".wish-button-play").on("click",function(){
+    video.play();
+});
+$(".wish-button-pause").on("click",function(){
+    video.pause();
 });
